5ac4361404e40d9dd06234ca91c36764487bce79
Bhuvan <144478466+Thor-asgardian@users.noreply.github.com>
2023-12-21 20:34:22 +0530

    Create sailor.sql

---
+++ sailor.sql
@@ -0,0 +1,54 @@
+CREATE TABLE sailor(
+sid INT PRIMARY KEY,
+sname VARCHAR(20),
+rating FLOAT,
+age INT
+);
+
+CREATE TABLE boat(
+bid INT PRIMARY KEY,
+bname VARCHAR(20),
+color VARCHAR(20)
+);
+
+CREATE TABLE rserver(
+sid INT,
+bid INT,
+dojoin DATE
+);
+
+INSERT INTO sailor (sid, sname, rating, age) VALUES (201, 'Albert', 7, 30);
+
+INSERT INTO sailor (sid, sname, rating, age) VALUES (202, 'Abrahim', 9, 32);
+
+INSERT INTO sailor (sid, sname, rating, age) VALUES (203, 'bhaskar', 6, 30);
+
+INSERT INTO sailor (sid, sname, rating, age) VALUES (204, 'Newton', 5, 30);
+
+INSERT INTO sailor (sid, sname, rating, age) VALUES (205, 'Einstien', 8, 30);
+
+SELECT * FROM sailor;
+
+INSERT INTO boat (bid, bname, color) VALUES (101, 'boat1', 'Yellow');
+
+INSERT INTO boat (bid, bname, color) VALUES (202, 'boat2', 'Red');
+
+INSERT INTO boat (bid, bname, color) VALUES (203, 'boat3', 'Green');
+
+INSERT INTO boat (bid, bname, color) VALUES (204, 'boat4', 'White');
+
+INSERT INTO boat (bid, bname, color) VALUES (205, 'boat5', 'Orange');
+
+SELECT * FROM boat;
+
+INSERT INTO rserver (sid, bid, dojoin) VALUES (201, 101, '2000-02-10');
+
+INSERT INTO rserver (sid, bid, dojoin) VALUES (202, 102, '2001-07-07');
+
+INSERT INTO rserver (sid, bid, dojoin) VALUES (203, 103, '2003-10-29');
+
+INSERT INTO rserver (sid, bid, dojoin) VALUES (204, 104, '2007-07-07');
+
+INSERT INTO rserver (sid, bid, dojoin) VALUES (205, 105, '2011-07-07');
+
+SELECT * FROM rserver;
